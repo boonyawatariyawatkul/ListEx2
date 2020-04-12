272177ca6c908a72c1166472d76d14d19e7eff7f
boonyawatariyawatkul <ariyawatkulb@gmail.com>
2020-04-12 15:13:09 +0700

    initial commit

---
+++ ListAndDictionaryExercise.py
@@ -0,0 +1,21 @@
+menuList = []
+
+
+def showBill():
+    text1 = "My Food"
+    print(text1.center(30,"-"))
+    totalPrice = 0
+    for food in range(len(menuList)):
+        print(menuList[food])
+        totalPrice += int(menuList[food][1])
+    print("Total = ", totalPrice)
+while True:
+    menuName = input("Please Enter Your Menu: ")
+    if (menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = input("Price: ")
+        menuList.append([menuName, menuPrice])
+
+print(menuList)
+showBill()
